07181df18dff922bc20667d6375c3b18274d0af6
get <gaetanpascal26740@gmail.com>
2020-05-14 15:31:35 +0200

    added minimun windows size added menu bar 3 actions added menu bar added tabs added buttons

--- gui.py
+++ gui.py
@@ -33,6 +33,27 @@ class Gui(QMainWindow):
         leaveMenu = mainMenu.addMenu('Leave')
 
 # ajouter description dans status bar
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(200, 200, 200, 200)
+
+        exitAction = QAction('&Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        self.setWindowTitle('PyQt5 Basic Menubar')
+        self.show()
 
 
 # ajouter onglet
@@ -60,7 +81,7 @@ def __init__(self, parent):
     self.layout.addWidget(self.tabs)
     self.setLayout(self.layout)
 
-    # ajouter bouton input dans l'onglet 1
+    # ajouter un bouton qui ouvre un "input file" pour demander le nom de l'utilisateur
 
     #Label's to fill widget
         self.label1 = QtWidgets.QLabel("Tab 1")
@@ -79,3 +100,52 @@ def __init__(self, parent):
         self.tab_widget.tabBar().setMovable(True)
 
         self.show()
+
+
+def __init__(self):
+    super().__init__()
+    self.title = 'PyQt5 file dialogs - pythonspot.com'
+    self.left = 10
+    self.top = 10
+    self.width = 640
+    self.height = 480
+    self.initUI()
+
+
+def initUI(self):
+    self.setWindowTitle(self.title)
+    self.setGeometry(self.left, self.top, self.width, self.height)
+
+    self.openFileNameDialog()
+    self.openFileNamesDialog()
+    self.saveFileDialog()
+
+    self.show()
+
+
+def openFileNameDialog(self):
+    options = QFileDialog.Options()
+    options |= QFileDialog.DontUseNativeDialog
+    fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "",
+                                              "All Files (*);;Python Files (*.py)", options=options)
+    if fileName:
+        print(fileName)
+
+
+def openFileNamesDialog(self):
+    options = QFileDialog.Options()
+    options |= QFileDialog.DontUseNativeDialog
+    files, _ = QFileDialog.getOpenFileNames(self, "QFileDialog.getOpenFileNames()", "",
+                                            "All Files (*);;Python Files (*.py)", options=options)
+    if files:
+        print(files)
+
+
+def saveFileDialog(self):
+    options = QFileDialog.Options()
+    options |= QFileDialog.DontUseNativeDialog
+    fileName, _ = QFileDialog.getSaveFileName(self, "QFileDialog.getSaveFileName()", "",
+                                              "All Files (*);;Text Files (*.txt)", options=options)
+    if fileName:
+        print(fileName)
+
